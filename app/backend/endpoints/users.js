83c57b3b013283217cfcc9969262be51eb8f3d46
Maksim Kanushin <rextuz@gmail.com>
2021-12-10 18:26:36 +0100

    Add new user to the database

--- app/backend/endpoints/users.js
+++ app/backend/endpoints/users.js
@@ -3,6 +3,26 @@ const app = module.exports = express();
 
 const User = require("../db/model/User");
 
+app.use(express.json());
+
 app.get("/api/users", (async (req, res) => {
     res.json(await User.findAll());
+}))
+
+app.post("/api/users", (async (req, res) => {
+    console.log(req.body);
+    let user = User.build({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: "password",
+        dateOfBirth: new Date("05.08.1985"),
+        gender: 0,
+        isStudent: true,
+        isTutor: true,
+        isAdmin: true,
+        status: 0,
+    });
+    await user.save();
+    res.json(user);
 }))
