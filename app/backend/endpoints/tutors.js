6190cae2bce9e177aa28edcb08b35f4ee694041e
Maksim Kanushin <mkanushin@cloudlinux.com>
2021-12-14 14:50:55 +0100

    Show all courses for the found tutor

--- app/backend/endpoints/tutors.js
+++ app/backend/endpoints/tutors.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = module.exports = express();
 
-const search = require("../utils/search");
+const search = require('../utils/search');
 
-const User = require("../db/model/User");
-const Course = require("../db/model/Course");
+const User = require('../db/model/User');
+const Course = require('../db/model/Course');
 
 app.get('/api/tutors', (async (req, res) => {
     let tutors = await User.findAll({
@@ -15,6 +15,16 @@ app.get('/api/tutors', (async (req, res) => {
             model: Course,
             where: search(req.query.search)
         }
-    })
+    });
+    let ids = [];
+    tutors.forEach(tutor => ids.push(tutor.id));
+    tutors = await User.findAll({
+        where: {
+            id: ids
+        },
+        include: {
+            model: Course,
+        }
+    });
     res.json(tutors);
 }))
