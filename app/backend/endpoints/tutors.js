326f112a725c69e8f1878d676e1a4986270bb03b
Maksim Kanushin <rextuz@gmail.com>
2021-12-22 14:18:18 +0100

    Save a tutor

--- app/backend/endpoints/tutors.js
+++ app/backend/endpoints/tutors.js
@@ -6,8 +6,9 @@ const search = require("../utils/search");
 const User = require("../db/model/User");
 const Course = require("../db/model/Course");
 
-const { SuccessfulResponse } = require("../utils/response");
+const { SuccessfulResponse, FailedResponse } = require("../utils/response");
 const { Op } = require("sequelize");
+const bcryptjs = require("bcryptjs");
 
 app.get("/api/tutors", async (req, res) => {
     let tutors = await User.findAll({
@@ -27,26 +28,35 @@ app.get("/api/tutors", async (req, res) => {
     res.json(new SuccessfulResponse("List of tutors", tutors));
 });
 
-// app.post("/api/tutors", async (req, res) => {
-//     let existing_user = await User.findOne({
-//         where: { email: req.body.email },
-//     });
-//     if (existing_user) {
-//         res.json({ error: "User with this email is already registered" });
-//         return;
-//     }
-//     let user = User.build({
-//         firstName: req.body.firstName,
-//         lastName: req.body.lastName,
-//         email: req.body.email,
-//         password: req.body.password,
-//         dateOfBirth: req.body.dateOfBirth,
-//         gender: req.body.gender,
-//         isStudent: false,
-//         isTutor: true,
-//         isAdmin: false,
-//         status: 0,
-//     });
-//     await user.save();
-//     res.json(user);
-// });
+app.post("/api/users", async (req, res) => {
+    let existing_user = await User.findOne({
+        where: { email: req.body.email },
+    });
+
+    if (existing_user) {
+        res.json(
+            new FailedResponse("User with this email is already registered")
+        );
+        return;
+    }
+
+    let epassword = await bcryptjs.hash(req.body.password, 10);
+
+    let user = User.build({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: epassword,
+        dateOfBirth: new Date(req.body.dateOfBirth),
+        gender: req.body.gender,
+        isStudent: false,
+        isTutor: true,
+        status: 0,
+    });
+
+    await user.save().catch((e) => {
+        console.log(e);
+    });
+
+    res.json(new SuccessfulResponse("Success", [user]));
+});
