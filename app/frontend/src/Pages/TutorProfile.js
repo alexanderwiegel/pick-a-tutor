9676559506f4a2d82f5e90317d21ac8920847df6
Alex <alexanderwiegel17@gmail.com>
2022-01-09 18:05:20 +0100

    Created basic files for the admin and chat pages

---
+++ app/frontend/src/Pages/TutorProfile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TutorProfile = () => {
+  return (
+    <div className='App'>
+
+    </div>
+  );
+};
+
+export default TutorProfile;
---
+++ app/frontend/src/Pages/Users.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Users = () => {
+  return (
+    <div className='App'>
+
+    </div>
+  );
+};
+
+export default Users;
