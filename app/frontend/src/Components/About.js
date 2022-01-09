d389eea615d92eb4dc540a311ec6355fa9365bc7
Alex <alexanderwiegel17@gmail.com>
2022-01-09 17:59:28 +0100

    Change indentation

--- app/frontend/src/Components/About.js
+++ app/frontend/src/Components/About.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-function About (props) {
-    return (
+function About(props) {
+  return (
     <div id='about' >
-        <div className= 'about-image' >
-            <img src = {props.image} alt='' />
-        </div>
-        <div className='about-text' >
-            <h2> {props.title} </h2>
-            <p>Instructors from around Fulda find hundered of students on Pick-A-Tutor. We provide the tools and skills to connect you to the students you want to help.</p>
-            <a href='/signup' className='cv-btn' style={{padding:6, textDecoration:'none'}}> {props.button} </a>
-        </div>
+      <div className='about-image' >
+        <img src={props.image} alt='' />
+      </div>
+      <div className='about-text' >
+        <h2> {props.title} </h2>
+        <p>Instructors from around Fulda find hundered of students on Pick-A-Tutor. We provide the tools and skills to connect you to the students you want to help.</p>
+        <a href='/signup' className='cv-btn' style={{ padding: 6, textDecoration: 'none' }}> {props.button} </a>
+      </div>
     </div>
-    )
+  )
 }
 
 export default About;
--- app/frontend/src/Components/Search.js
+++ app/frontend/src/Components/Search.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function Search () {
-    const [search, setSearch] = useState('');
-    const [star, setStar] = useState(0);
-   return(
-   <div id= 'search'>
-       <h3>Try searching for your subject</h3 >
-       <div className = 'search-input' >
-            <input placeholder = 'Search here ...' style={{fontSize:'1rem'}} onChange= { (e) => setSearch(e.target.value) }/>
-            
-            {/* <select name="pets" onChange={ (e) => setStar(e.target.value)}>
+function Search() {
+  const [search, setSearch] = useState('');
+  const [star, setStar] = useState(0);
+  return (
+    <div id='search'>
+      <h3>Try searching for your subject</h3 >
+      <div className='search-input' >
+        <input placeholder='Search here ...' style={{ fontSize: '1rem' }} onChange={(e) => setSearch(e.target.value)} />
+
+        {/* <select name="pets" onChange={ (e) => setStar(e.target.value)}>
                 <option value="0">Based on given Stars</option>
                 <option value="one">1 Star Rating</option>
                 <option value="two">2 Star Rating</option>
@@ -18,16 +18,16 @@ function Search () {
                 <option value="four">4 Star Rating</option>
                 <option value="five">5 Star Rating</option>
             </select> */}
-            {/* <a href = './Detail' > Search </a> */}
-            <Link to={'/detail' } state={{
-                // star : star, 
-                search : search
-            }} style={{textDecoration:'none'}}>Search</Link>
-       </div>
-       {/* <text>you selected {search} with star value {star} </text> */}
-       <text style={{ marginTop: '1%',color: '#ffffff',fontSize:'1rem'}}> © Copyright {new Date().getFullYear()} - Fulda University of Applied Sciences. All Rights Reserved.</text>
+        {/* <a href = './Detail' > Search </a> */}
+        <Link to={'/detail'} state={{
+          // star : star, 
+          search: search
+        }} style={{ textDecoration: 'none' }}>Search</Link>
+      </div>
+      {/* <text>you selected {search} with star value {star} </text> */}
+      <text style={{ marginTop: '1%', color: '#ffffff', fontSize: '1rem' }}> © Copyright 2021 - {new Date().getFullYear()} - Fulda University of Applied Sciences. All Rights Reserved.</text>
     </div>
-    )
+  )
 }
 
 export default Search;
