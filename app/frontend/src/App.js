256c472667dbdf7fc5558dc9fabc70ac958bbb4f
Alex <alexanderwiegel17@gmail.com>
2022-01-10 13:21:32 +0100

    Change routes

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import Details from './Components/Detail';
-import Login from './Components/Login';
+
+import SignUp from './Pages/SignUp';
+import Login from './Pages/Login';
 import PageTemplate from './Components/PageTemplate';
-import Home from './Components/Home';
-import SignUp from './Components/SignUp';
-import Dashboard from './Components/Dashboard';
+import LandingPage from './Pages/LandingPage';
+import Home from './Pages/Home';
+import Results from './Pages/Results';
+import TutorProfile from './Pages/TutorProfile';
+import Users from './Pages/Users';
+import Approvals from './Pages/Approvals';
+import Messages from './Pages/Messages';
 import Footer from './Components/Footer';
+
 import { BrowserRouter as Router, Routes, Route, Navigate, Switch, useLocation } from 'react-router-dom';
 
 
 function App() {
   return (
     <Router>
-        <Routes>
-          <Route path = '/' element = { <PageTemplate><Home /></ PageTemplate> } />
-          <Route exact path = '/login' element = { <Login /> } />
-          <Route exact path = '/signup' element = { <SignUp /> } />
-          <Route exact path = '/detail' element = { <PageTemplate><Details /><Footer/></PageTemplate> } />
-          <Route exact path = '/dashboard' element = { <PageTemplate><Dashboard /></PageTemplate> } />
-        </Routes>
+      <Routes>
+        {/* TODO: set this back to LandingPage or Home or TutorProfile or Approvals */}
+        <Route path='/' element={<PageTemplate><Approvals /></ PageTemplate>} />
+        <Route exact path='/signup' element={<SignUp />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/home' element={<PageTemplate><Home /></PageTemplate>} />
+        <Route exact path='/results' element={<PageTemplate><Results /><Footer /></PageTemplate>} />
+        <Route exact path='/tutorProfile' element={<PageTemplate><TutorProfile /></PageTemplate>} />
+        <Route exact path='/users' element={<PageTemplate><Users /></PageTemplate>} />
+        <Route exact path='/approvals' element={<PageTemplate><Approvals /></PageTemplate>} />
+        <Route exact path='/messages' element={<PageTemplate><Messages /></PageTemplate>} />
+      </Routes>
     </Router>
   );
 }
