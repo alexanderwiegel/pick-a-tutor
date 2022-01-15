827a73bf750e4b1db6092164b6d7cc38ca232a72
Alex <alexanderwiegel17@gmail.com>
2022-01-15 15:56:50 +0100

    Add Users page with UserCard

---
+++ app/frontend/src/Components/UserCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { ButtonGroup } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+
+function UserCard({ user }) {
+  return (
+    <>
+      <Card style={{ width: '30rem' }}>
+        <Card.Body>
+          {/* <Card.Img src={user.profilePic} /> */}
+          <Card.Title><Link to="">{user.firstName} {user.lastName}</Link></Card.Title>
+          {/* TODO: base subtitle on role (e.g. with user.isStudent) */}
+          <Card.Subtitle>Tutor</Card.Subtitle>
+          <ButtonGroup className="d-flex">
+            <Button>Contact</Button>
+            <Button variant="danger">Block</Button>
+          </ButtonGroup>
+        </Card.Body>
+      </Card>
+    </>
+  )
+}
+
+export default UserCard;
