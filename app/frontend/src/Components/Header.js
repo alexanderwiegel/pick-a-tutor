9b299f0006a4cbc8e80bec9ba111bae4f5e8f080
Alexander Wiegel <alexanderwiegel17@gmail.com>
2022-01-15 19:15:05 +0100

    Fix indentation and name

--- app/frontend/src/Components/Header.js
+++ app/frontend/src/Components/Header.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import Navbar from './Navbar';
 
-function Header () {
-    return(
-        <div id='main'>
-            
-            <div className='name'>
-                <h1>New to <span>Pick A Tutor</span>? Lucky you.</h1>
-                <p className='details'>We strive to bring you the best tutors available near you, with their broad selection of offered courses.</p>
-                <a href = '/signup' className='cv-btn' style={{textDecoration:'none', width:'30%'}} >SignUp Now</a>
-            </div>
-        </div>
-    );
+function Header() {
+  return (
+    <div id='main'>
+      <div className='name'>
+        <h1>New to <span>Pick-A-Tutor</span>? Lucky you.</h1>
+        <p className='details'>We strive to bring you the best tutors available near you, with their broad selection of offered courses.</p>
+        <a href='/signup' className='cv-btn' style={{ textDecoration: 'none', width: '30%' }} >SignUp Now</a>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
