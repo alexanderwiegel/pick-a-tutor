16c17e8fd99cabfb26ce0d0d72f020eebf1d0dd2
DESKTOP-S9REJE4\Shahryar <abdullah.butt@msn.com>
2022-01-15 12:44:45 +0100

    Tutor Course Post

--- app/backend/endpoints/tutorcourses.js
+++ app/backend/endpoints/tutorcourses.js
@@ -5,16 +5,27 @@ const search = require("../utils/search");
 // const auth = require("../auth/check-auth");
 const { Sequelize } = require("sequelize");
 const jwtdecode = require("jwt-decode");
+const jwt = require("jsonwebtoken");
 const Op = Sequelize.Op;
 
 //****************** Get All Courses of All Tutors *******************
 const getAllCourses = async(req,res) => {
+/*
+    const tutorCourses = await TutorCourse.findAll({
+        where: search(req.query.search, "CourseId"),
+        include: [Course]
+    });
 
     res.json({
         success: true,
         message: "List of all Courses",
-        //records: courses.length,
-        //data: courses,
+        records: tutorCourses.length,
+        data: tutorCourses,
+    });
+*/
+    res.json({
+        success: true,
+        message: "List of all Courses",
     });
 
 };
@@ -38,11 +49,70 @@ exports.getTutorCourses = getTutorCourses;
 
 const postTutorCourse = async(req,res) => {
 
+    var courseId = 0;
+
+    const existingCourse = await Course.findOne({
+        //where: search(req.query.search, "id"),
+        where: search(req.body.name, "name"),
+    });
+
+    if (existingCourse){
+        courseId = existingCourse.id;
+/*
+        res.json({
+            success: true,
+            message: "Course exists",
+            records: existingCourse.length,
+            data: existingCourse,
+        });
+*/
+    }
+    else {
+        let course = Course.build({
+            name: req.body.name,
+            // Since we do not have a description, putting the name of the course in the description
+            description: req.body.name,
+        });
+
+        await course.save().catch((e) => {
+            console.log(e);
+        });
+
+        courseId = course.id;
+    }
+    var token = req.headers.authorization.split(" ")[2]; // Bearer Token
+
+    const decodedToken = jwt.verify(token, "privatekey");
+    const UserID = decodedToken.id;
+/*
     res.json({
         success: true,
-        message: "Tutor Course Created",
-        //records: courses.length,
-        //data: courses,
+        message: "Test",
+        data: "courseId: " + courseId,
+    });
+*/
+    let tutorCourse = TutorCourse.build({
+        UserId: UserID,
+        CourseId: courseId,
+        coursePricePerHour: req.body.coursePricePerHour,
+        isFull: 0
+    });
+/*
+    res.json({
+        success: true,
+        message: "Test",
+        data: tutorCourse,
+    });
+*/
+    await tutorCourse.save().catch((e) => {
+        console.log(e);
+    });
+
+    res.json({
+        success: true,
+        message: "Tutor Course " + req.body.name + " Created",
+        records: tutorCourse.length,
+        data: tutorCourse,
     });
 };
 
