7f6b7567977043bb4f915b43e369be8925c2e518
fdai7180 <chahat.soni@informatik.hs-fulda.de>
2022-01-16 17:53:08 +0100

    Changes to the editor

--- app/frontend/src/Components/Feature.js
+++ app/frontend/src/Components/Feature.js
@@ -2,7 +2,6 @@ import React from 'react';
 import FeatureBox from './FeatureBox';
 import featureimage from '../images/feature_1.png';
 import featureimage1 from '../images/feature_2.png';
-import featureimage2 from '../images/feature_3.png';
 
 function Feature() {
   return (
