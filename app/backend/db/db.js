e5a2f2e0f7b3140088bf3befa0bde2086adf70ce
Ziad Khaled <ziadkhaled1419@gmail.com>
2022-01-23 14:23:42 +0100

    Update database ip address

--- app/backend/db/db.js
+++ app/backend/db/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize("db", "mysqluser", "attic-humorous-stylishly", {
-    host: "20.113.25.17",
+    host: "20.113.56.213",
     port: 3306,
     dialect: "mysql",
 });
