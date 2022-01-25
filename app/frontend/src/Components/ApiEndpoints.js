9eac42534237b0931c25baf7451cf6f2c3a78223
fdai7180 <chahat.soni@informatik.hs-fulda.de>
2022-01-25 11:24:34 +0100

    Changed API endpoint baseURL to remote

--- app/frontend/src/Components/ApiEndpoints.js
+++ app/frontend/src/Components/ApiEndpoints.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:3001/api",
+  baseURL: "http://20.113.25.17:3001/api",
   headers: { "Authorization": `Bearer ${localStorage.getItem("token")}` }
 })
 
