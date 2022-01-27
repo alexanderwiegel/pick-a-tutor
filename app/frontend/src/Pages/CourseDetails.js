1265baf28ebef136566a948fe7ad68ab75fdfde2
Ziad Khaled <ziadkhaled1419@gmail.com>
2022-01-27 02:15:59 +0100

    Show the lastest reviews on top

--- app/frontend/src/Pages/CourseDetails.js
+++ app/frontend/src/Pages/CourseDetails.js
@@ -131,7 +131,7 @@ function CourseDetails() {
 
           <div>
             {/* TODO: check the correct way to get the unreported reviews */}
-            {courseDetails.Reviews.filter((review) => review.reportReview === null).map((review) => (
+            {courseDetails.Reviews.filter((review) => review.reportReview === null).reverse().map((review) => (
               <Review review={review} key={review.id} />
             ))}
           </div>
